9a01ff48e824c84c0dd9814fb6a46f1ca8e8fc6a
aliasgermodi <modialiasger@gmail.com>
2016-02-06 13:58:01 +0530

    First commit, trying ListView

---
+++ MovieList/app/src/main/java/com/example/ali/movielist/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ali.movielist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    //Array of strings...
+    String[] mobileArray = {"Android", "Iphone", "WindowsMobile", "Blackberry"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_main, mobileArray);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+    }
+
+
+
+
+}
