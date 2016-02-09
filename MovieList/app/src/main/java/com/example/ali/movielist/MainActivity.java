704d66e5dd7762e8510b28ae51b14f09a3efd07f
aliasgermodi <modialiasger@gmail.com>
2016-02-09 11:48:33 +0530

    Changed the layouts, click on one listView and open new Activity

--- MovieList/app/src/main/java/com/example/ali/movielist/MainActivity.java
+++ MovieList/app/src/main/java/com/example/ali/movielist/MainActivity.java
@@ -1,28 +1,46 @@
 package com.example.ali.movielist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    //Array of strings...
-    String[] mobileArray = {"Android", "Iphone", "WindowsMobile", "Blackberry"};
-
-
+public class MainActivity extends ListActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_main2, mobileArray);
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
+        // storing string resources into Array
+        String[] adobe_products = getResources().getStringArray(R.array.adobe_products);
+
+        // Binding resources Array to ListAdapter
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.label, adobe_products));
+
+        ListView lv = getListView();
+
+        // listening to single list item on click
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // selected item
+                String product = ((TextView) view).getText().toString();
+
+                // Launching new Activity on selecting single List Item
+                Intent i = new Intent(getApplicationContext(), SingleListItem.class);
+                // sending data to new activity
+                i.putExtra("product", product);
+                startActivity(i);
+
+            }
+        });
     }
-
-
 
 
 }
